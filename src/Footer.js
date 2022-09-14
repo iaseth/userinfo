15b4a73c8ae5304d2a963944f7f71cac1e6c01a7
Ankur Seth <iankurseth@gmail.com>
2022-09-14 17:13:35 +0530

    Small changes to Header and Footer

--- src/Footer.js
+++ src/Footer.js
@@ -4,8 +4,8 @@
 function Footer () {
 	return (
 		<footer className="Footer">
-			<div className="bg-slate-800 text-white px-8 py-20 text-center">
-				<h1 className="text-3xl font-bold">Footer</h1>
+			<div className="bg-slate-900 text-white px-8 py-20 text-center">
+				<h1 className="text-2xl font-bold">Footer</h1>
 			</div>
 		</footer>
 	);
--- src/Header.js
+++ src/Header.js
@@ -4,8 +4,8 @@
 function Header () {
 	return (
 		<header className="Header">
-			<div className="bg-red-700 text-white px-8 py-10 text-center">
-				<h1 className="text-2xl font-bold">Header</h1>
+			<div className="bg-slate-700 text-white px-8 py-10 text-center">
+				<h1 className="text-xl font-bold">UserInfo</h1>
 			</div>
 		</header>
 	);
