c018526bcc878551a4f6004ee39eda0ddac8f5fc
Ankur Seth <iankurseth@gmail.com>
2022-10-29 18:29:03 +0530

    Moved Header and Footer inside UserInfo

--- src/App.js
+++ src/App.js
@@ -1,14 +1,11 @@
-import Header from './Header';
-import Footer from './Footer';
 import UserInfo from './UserInfo';
+
 
 
 function App () {
 	return (
 		<div className="App">
-			<Header />
 			<UserInfo />
-			<Footer />
 		</div>
 	);
 }
--- src/UserInfo/UserInfo.js
+++ src/UserInfo/UserInfo.js
@@ -1,15 +1,21 @@
+import Header from './Header';
+import Footer from './Footer';
 import DebugInfo from './DebugInfo';
 
 
 
 export default function UserInfo () {
 	return (
-		<div className="UserInfo min-h-screen bg-slate-200 px-2 py-16">
-			<main className="max-w-xl mx-auto">
-				<h1 className="text-3xl text-center font-bold m-auto">userInfo</h1>
-				<DebugInfo object={navigator} />
-				<DebugInfo object={window} />
-			</main>
+		<div className="UserInfo">
+			<Header />
+			<div className="UserInfo min-h-screen bg-slate-200 px-2 py-16">
+				<main className="max-w-xl mx-auto">
+					<h1 className="text-3xl text-center font-bold m-auto">userInfo</h1>
+					<DebugInfo object={navigator} />
+					<DebugInfo object={window} />
+				</main>
+			</div>
+			<Footer />
 		</div>
 	);
 }
